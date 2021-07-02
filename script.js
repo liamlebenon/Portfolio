664231f66d6243f2612adbf392f64b434edf4d4f
unknown <liam.lebenon@gmail.com>
2021-07-02 11:11:34 +0100

    add navbar and landing

---
+++ script.js
@@ -0,0 +1,12 @@
+//Script for menu icon
+let menuIcon = document.getElementById("menuIcon");
+let navBar = document.getElementById("navBar");
+let hideBtn = document.getElementById("hideNavBtn");
+
+function changeNav() {
+    navBar.style.display = "block";
+}
+
+function closeNav(){
+    navBar.style.display = "none";
+}

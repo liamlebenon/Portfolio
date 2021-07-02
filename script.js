7efd7fab7ec4f9e0f9be7cc7013814815fdb40c5
unknown <liam.lebenon@gmail.com>
2021-07-02 14:08:46 +0100

    update site 50% complete

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ let hideBtn = document.getElementById("hideNavBtn");
 
 function changeNav() {
     navBar.style.display = "block";
+    menuIcon.style.display = "none";
 }
 
 function closeNav(){
     navBar.style.display = "none";
+    menuIcon.style.display = "inline-block";
 }

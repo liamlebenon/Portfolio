8fb50c435d9224a8dbb42c742a7be51bec244496
unknown <liam.lebenon@gmail.com>
2021-07-02 22:31:00 +0100

    clean JS and add new mobile menu feature

--- script.js
+++ script.js
@@ -2,13 +2,32 @@
 let menuIcon = document.getElementById("menuIcon");
 let navBar = document.getElementById("navBar");
 let hideBtn = document.getElementById("hideNavBtn");
+let nav = document.getElementById("nav");
+let windowSize = window.matchMedia( "(max-width: 1024px)" );
 
+//Function to open the nav bar
 function changeNav() {
-    navBar.style.display = "block";
-    menuIcon.style.display = "none";
+        navBar.style.display = "block";
+        menuIcon.style.display = "none";
 }
 
-function closeNav(){
+menuIcon.addEventListener("click", changeNav);
+
+//function to close the nav bar
+function closeNav() {    
     navBar.style.display = "none";
     menuIcon.style.display = "inline-block";
 }
+
+hideNavBtn.addEventListener("click", closeNav);
+
+//Function for mobile nav menu
+function mobileCloseNav(){
+    if(windowSize.matches) {
+        navBar.style.display = "none";
+        menuIcon.style.display = "inline-block";
+    }
+}
+
+document.querySelectorAll(".navOption").forEach(elem => elem.addEventListener("click", mobileCloseNav));
+//End of mobile nav menu function
